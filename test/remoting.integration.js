a99db132f364d8ba681a4044fe8c6b2fd90a49de
Amir Jafarian <amirali.jfr61@gmail.com>
2016-05-10 16:24:14 -0400

    Comments

--- test/remoting.integration.js
+++ test/remoting.integration.js
@@ -119,7 +119,8 @@ describe('remoting - integration', function() {
       // TODO: This is the list of expected default endpoints
       // for LB 2.X (They are different for LB 3.X)
       var expectedMethods = [
-        'create(data:object):store POST /stores',
+        'create(data:object):store POST /stores', // 2.X
+        // 'upsert(data:object):store PATCH /stores', // 3.X
         'upsert(data:object):store PUT /stores',
         'exists(id:any):boolean GET /stores/:id/exists',
         'findById(id:any,filter:object):store GET /stores/:id',
@@ -127,10 +128,13 @@ describe('remoting - integration', function() {
         'findOne(filter:object):store GET /stores/findOne',
         'updateAll(where:object,data:object):object POST /stores/update',
         'deleteById(id:any):object DELETE /stores/:id',
-        'replaceById(id:any,data:object):store PATCH /stores/:id',
-        'replaceOrCreate(data:object):store PATCH /stores',
+        'replaceById(id:any,data:object):store PATCH /stores/:id', // 2.X
+        // 'replaceById(id:any,data:object):store PUT /stores/:id', // 3.X
+        'replaceOrCreate(data:object):store PATCH /stores', // 2.X
+        // 'replaceOrCreate(data:object):store PUT /stores', // 3.X
         'count(where:object):number GET /stores/count',
-        'prototype.updateAttributes(data:object):store PUT /stores/:id',
+        'prototype.updateAttributes(data:object):store PUT /stores/:id', // 2.X
+        // 'prototype.updateAttributes(data:object):store PATCH /stores/:id', // 3.X
         'createChangeStream(options:object):ReadableStream POST /stores/change-stream',
       ];
 
